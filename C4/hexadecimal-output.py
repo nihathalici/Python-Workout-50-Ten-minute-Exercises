ece92716c0923df95debb75fa9cedbc23118731f
Nihat Halici <nihat.halici@gmail.com>
2021-09-01 03:08:57 -0700

    Delete C4 directory

--- C4/hexadecimal-output.py
+++
@@ -1,8 +0,0 @@
-def hex_output():
-    decnum = 0
-    hexnum = input('Enter a hex number to convert: ')
-    for power, digit in enumerate(reversed(hexnum)):
-        decnum += int(digit, 16) * (16 ** power)
-    print(decnum)
-
-hex_output()
