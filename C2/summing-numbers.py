7f2cdb7b8481fd1f19020ae96a828dc6f7065448
Nihat Halici <nihat.halici@gmail.com>
2021-09-01 03:08:23 -0700

    Delete C2 directory

--- C2/summing-numbers.py
+++
@@ -1,7 +0,0 @@
-def mysum(*numbers):
-    output = 0
-    for number in numbers:
-        output += number
-    return output
-
-print(mysum(10, 20, 30, 40))
--- C2/summing-numbers_BTE_1.py
+++
@@ -1,7 +0,0 @@
-def mysum(li, num = 0):
-    output = 0
-    for item in li:
-        output += item
-    return output + num
-
-print(mysum([1, 2, 3], 4))
