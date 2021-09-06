d654965d3a49b1705d47f5ab1eeff57c77132122
Nihat Halici <nihat.halici@gmail.com>
2021-09-06 01:58:47 -0700

    Create mysum.py

---
+++ CHPT-03-Lists-and-tuples/Exer-10-Summing-anything/mysum.py
@@ -0,0 +1,13 @@
+
+def mysum(*items):
+    if not items:
+        return items
+    output = items[0]
+    for item in items[1:]:
+        output += item
+    return output
+
+print(mysum())
+print(mysum(10, 20, 30, 40))
+print(mysum('a', 'b', 'c', 'd'))
+print(mysum([10, 20, 30], [40, 50, 60], [70, 80]))
