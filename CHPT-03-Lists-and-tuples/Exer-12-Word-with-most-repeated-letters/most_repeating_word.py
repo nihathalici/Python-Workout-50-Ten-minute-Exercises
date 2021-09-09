ee465d96f004b30af561409983139355bbdac63f
Nihat Halici <nihat.halici@gmail.com>
2021-09-09 02:26:50 -0700

    Create most_repeating_word.py

---
+++ CHPT-03-Lists-and-tuples/Exer-12-Word-with-most-repeated-letters/most_repeating_word.py
@@ -0,0 +1,15 @@
+
+from collections import Counter
+import operator
+
+WORDS = ['this', 'is', 'an', 'elementary', 'test', 'example']
+
+def most_repeating_letter_count(word):
+    return Counter(word).most_common(1)[0][1]
+
+
+def most_repeating_word(words):
+    return max(words,
+               key=most_repeating_letter_count)
+
+print(most_repeating_word(WORDS))
