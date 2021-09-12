99edd3bb45619b479725dbadfbcfca73e6adb84d
Nihat Halici <nihat.halici@gmail.com>
2021-09-12 12:36:35 +0200

    Add files via upload

---
+++ CHPT-03-Lists-and-tuples/Exer-13-Printing-tuple-records/BTE_1_alternative_version.py
@@ -0,0 +1,22 @@
+import collections
+from operator import itemgetter
+
+PEOPLE = [('Donald', 'Trump', 7.85),
+          ('Vladimir', 'Putin', 3.626),
+          ('Jinping', 'Xi', 10.603)]
+
+
+def format_sort_records(iter):
+    output = []
+    template = '{1:10} {0:10} {2:5.2f}'
+
+    person = collections.namedtuple('person',('firstname','lastname','hours'))
+    persons = [person(p[0],p[1],p[-1]) for p in iter]
+
+    for one_person in sorted(persons, key=itemgetter(1, 0)):
+        output.append(template.format(*one_person))
+    
+    return output
+
+print('\n'.join(format_sort_records(PEOPLE)))
+
