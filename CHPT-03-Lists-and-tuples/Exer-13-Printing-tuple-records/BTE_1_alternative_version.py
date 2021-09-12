24f38eb7a5ad74edf2fcd19836160f1a6f6cecb5
Nihat Halici <nihat.halici@gmail.com>
2021-09-12 03:40:57 -0700

    Update BTE_1_alternative_version.py

--- CHPT-03-Lists-and-tuples/Exer-13-Printing-tuple-records/BTE_1_alternative_version.py
+++ CHPT-03-Lists-and-tuples/Exer-13-Printing-tuple-records/BTE_1_alternative_version.py
@@ -1,3 +1,9 @@
+'''
+If you find tuples annoying because they use numeric indexes, you’re not alone! Reimplement this exercise 
+using namedtuple objects (http://mng.bz/gyWl), defined in the collections module. Many people like to use 
+named tuples because they give the right balance between readability and efficiency.
+'''
+
 import collections
 from operator import itemgetter
 
