84d593682e05bb6393ad858e855575a460775c61
Nihat Halici <nihat.halici@gmail.com>
2021-09-22 10:49:25 +0200

    Add files via upload

--- CHPT-05-Files/Exer-20-Word-count/wcount.py
+++ CHPT-05-Files/Exer-20-Word-count/wcount.py
@@ -6,6 +6,30 @@ def wcount(fname):
               'lines': 0}
     unq_wo = set()
 
+    for x in open(fname):
+        cnts['lines'] += 1
+        cnts['characters'] += len(x)
+        cnts['words'] += len(x.split())
+
+        unq_wo.update(x.split())
+
+    print(unq_wo)
+
+    cnts['unique words'] = len(unq_wo)
+
+    for k, v in cnts.items():
+        print(f'{k}: {v}')
+
+    
+
+'''
+def wcount(fname):
+
+    cnts = {'characters': 0,
+              'words': 0,
+              'lines': 0}
+    unq_wo = set()
+
     for one_line in open(fname):
         cnts['lines'] += 1
         cnts['characters'] += len(one_line)
@@ -17,5 +41,6 @@ def wcount(fname):
 
     for k, v in cnts.items():
         print(f'{k}: {v}')
+'''
 
 wcount('wcountfile.txt')
---
+++ CHPT-05-Files/Exer-20-Word-count/wcount_BTE_1.py
@@ -0,0 +1,24 @@
+'''
+Ask the user to enter the name of a text file and then (on one line,
+separated by spaces) words whose frequencies should be counted in that file.
+Count how many times those words appear in a dict, using the user-entered
+words as the keys and the counts as the values.
+'''
+
+from os import path
+
+def cnt_wrd():
+
+    val = input('> ').split()
+    fname = val[0]
+
+    wrd_cnt_di = {k: 0 for k in val[1:]}
+    with open(fname) as f:
+        for l in f:
+            wrds = l.strip().split()
+            for wrd in wrds:
+                if wrd in wrd_cnt_di:
+                    wrd_cnt_di[wrd] += 1
+    print(wrd_cnt_di)
+
+cnt_wrd()
