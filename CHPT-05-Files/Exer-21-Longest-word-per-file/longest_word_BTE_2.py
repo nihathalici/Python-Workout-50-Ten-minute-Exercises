54a31167efd8ee4afc2ec1e616fe3a061c5ab3cb
Nihat Halici <nihat.halici@gmail.com>
2021-09-23 10:08:55 +0200

    Add files via upload

---
+++ CHPT-05-Files/Exer-21-Longest-word-per-file/longest_word_BTE_2.py
@@ -0,0 +1,23 @@
+"""
+Ask the user for a directory name. Show all of the files in the directory, as well
+as how long ago the directory was modified. You will probably want to use a
+combination of os.stat and the Arrow package on PyPI (http://mng.bz/nPPK)
+to do this easily.
+"""
+
+from pathlib import Path
+from datetime import datetime
+    
+def modfy_tme(path='.'):
+    
+    res_di = {}
+
+    with Path(path) as dir:
+        res_di['DIR'] = str(dir.cwd())
+        res_di['DLM'] = datetime.fromtimestamp(dir.stat().st_mtime).ctime()
+        for file in dir.iterdir():
+            if file.is_file():
+                res_di['F'] = list(res_di.get('F', [])) + [[ file.name, datetime.utcfromtimestamp(file.stat().st_mtime).ctime() ]]
+    return res_di
+
+print(modfy_tme())
---
+++ CHPT-05-Files/Exer-21-Longest-word-per-file/longest_word_BTE_3.py
@@ -0,0 +1,16 @@
+"""
+Open an HTTP server’s log file. (If you lack one, then you can read one from
+me at http://mng.bz/vxxM.) Summarize how many requests resulted in numeric
+response codes—202, 304, and so on.
+"""
+
+def log_chq(fname):
+
+    cnt_di = {}
+    with open(fname) as f:
+        for l in f:
+            l = l.split()
+            cnt_di[l[8]] = cnt_di.get(l[8], 0) + 1
+    return cnt_di
+
+print(log_chq('mini-access-log.txt'))
