a40582b81b7d0d4957ac507723b316a08b14d366
Nihat Halici <nihat.halici@gmail.com>
2021-09-25 09:42:28 +0200

    Add files via upload

---
+++ CHPT-05-Files/Exer-22-Reading-and-writing-CSV/passwd_to_csv_BTE_1.py
@@ -0,0 +1,35 @@
+'''
+Extend this exercise by asking the user to enter a space-separated list of integers,
+indicating which fields should be written to the output CSV file. Also ask the user
+which character should be used as a delimiter in the output file. Then read from
+/etc/passwd, writing the user’s chosen fields, separated by the user’s chosen delimiter.
+'''
+
+import csv
+
+def fnd_ln(inpfile):
+    with open(inpfile) as f:
+        rd = csv.reader(f, delimiter=':')
+        for l in rd:
+            if len(l) > 2:
+                return len(l)
+        return 0
+
+def usr_csv(inpfile, outfile):
+    with open(inpfile) as csv_rd, open(outfile,'w') as csv_wr:
+        rw_ln = fnd_ln(inpfile)
+
+        fld = input('Which columns? : ')
+        dlm = input('Which delimiter? : ')
+        fld = list(filter(lambda x: x < rw_ln, [abs(int(x)) for x in fld.split()]))
+
+        wr = csv.writer(csv_wr, delimiter=dlm)
+        wr.writerow([f'Column numbers: {fld}'])
+
+        rd = csv.reader(csv_rd, delimiter=':',)
+        for l in rd:
+            if len(l) > 1: wr.writerow([l[col] for col in fld])
+        print('DONE')
+
+usr_csv(inpfile='linux-etc-passwd.txt', outfile='password_out.csv')
+                
---
+++ CHPT-05-Files/Exer-22-Reading-and-writing-CSV/passwd_to_csv_BTE_2.py
@@ -0,0 +1,18 @@
+'''
+Write a function that writes a dict to a CSV file. Each line in the CSV file
+should contain three fields: (1) the key, which we’ll assume to be a string,
+(2) the value, and (3) the type of the value (e.g., str or int).
+'''
+
+import csv
+
+def csv_from_di(res_file='di_val_typ.csv'):
+    di_val = { 'first': '1', 'second': 2, 'three': [3, 4, '5'], 'fourth': True, 'fifth': (6, 7) }
+    with open(res_file, 'w') as csv_wr:
+        wr = csv.writer(csv_wr)
+        for k in di_val:
+            wr.writerow([k, di_val[k], type(di_val[k])])
+        
+
+
+csv_from_di()
