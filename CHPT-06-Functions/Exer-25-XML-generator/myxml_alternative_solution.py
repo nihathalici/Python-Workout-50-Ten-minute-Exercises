c74cf4f91ce56c6e4b1848178c5bc2d42a2236a8
Nihat Halici <nihat.halici@gmail.com>
2021-09-29 20:09:23 +0200

    Add files via upload

---
+++ CHPT-06-Functions/Exer-25-XML-generator/myxml_alternative_solution.py
@@ -0,0 +1,11 @@
+
+def myxml(*args, **kwargs):
+    tag, content = " ".join(args[:1]), ". ".join(args[1:])
+    attr_di = [f'{k}="{v}"' for k, v in kwargs.items()]
+    attr_di.insert(0, tag)
+    return f'<{" ".join(attr_di)}>{content}</{tag}>'
+
+
+print(myxml('foo'))
+print(myxml('foo', 'text'))
+print(myxml('foo', 'bar', a=1, b=2, c=3) )
