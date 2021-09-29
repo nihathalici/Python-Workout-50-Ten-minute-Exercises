54f55520f17669418aacabde2f97f32d0ba07d8d
Nihat Halici <nihat.halici@gmail.com>
2021-09-29 19:20:23 +0200

    Add files via upload

---
+++ CHPT-06-Functions/Exer-25-XML-generator/myxml.py
@@ -0,0 +1,9 @@
+
+def myxml(tagname, content='', **kwargs):
+    attributes = ''.join(f' {key}="{value}"' for key, value in kwargs.items())
+    
+    return f'<{tagname}{attributes}>{content}</{tagname}>'
+
+print(myxml('foo'))
+print(myxml('foo', 'text'))
+print(myxml('foo', 'bar', a=1, b=2, c=3) )
