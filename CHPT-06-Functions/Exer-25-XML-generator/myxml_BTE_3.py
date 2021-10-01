72df4b096a4d42c4a01c14cf47138223440a7cb4
Nihat Halici <nihat.halici@gmail.com>
2021-10-01 10:24:08 +0200

    Add files via upload

---
+++ CHPT-06-Functions/Exer-25-XML-generator/myxml_BTE_3.py
@@ -0,0 +1,16 @@
+'''
+Write an anyjoin function that works similarly to str.join, except that the first
+argument is a sequence of any types (not just of strings), and the second argument
+is the “glue” that we put between elements, defaulting to " " (a space).
+
+So anyjoin([1,2,3]) will return 1 2 3,
+and anyjoin('abc', pass:'**') will return pass:a**b**c.
+'''
+
+def anyjoin(iter, glue=' '):
+    return glue.join([str(x) for x in iter])
+
+
+print(anyjoin([1,2,3]))
+
+print(anyjoin('abc', glue='**'))
