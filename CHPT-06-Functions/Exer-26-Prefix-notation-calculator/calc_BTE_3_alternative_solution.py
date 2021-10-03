97eab6736284c47fae3ae85399455dead5b821b1
Nihat Halici <nihat.halici@gmail.com>
2021-10-03 16:23:53 +0200

    Add files via upload

---
+++ CHPT-06-Functions/Exer-26-Prefix-notation-calculator/calc_BTE_3_alternative_solution.py
@@ -0,0 +1,16 @@
+
+'''
+Write a function, transform_lines, that takes three arguments: a function
+that takes a single argument, the name of an input file, and the name of
+an output file. Calling the function will run the function on each line
+of the input file, with the results written to the output file.
+
+(Hint: the previous exercise and this one are closely related.)
+'''
+
+def transform_lines(fun, f_in, f_out):
+    with open(f_in) as f_rd, open(f_out, 'w') as f_wr:
+        for l in f_rd:
+            f_wr.write(fun(l))
+
+transform_lines((lambda x: x.lower()), 'sonnet_126.txt', 'output_2.txt')
