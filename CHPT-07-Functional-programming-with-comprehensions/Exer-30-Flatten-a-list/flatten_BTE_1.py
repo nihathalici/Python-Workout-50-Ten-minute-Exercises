6cea9130b374509881894a395031ecfa58bf8654
Nihat Halici <nihat.halici@gmail.com>
2021-10-16 13:16:57 +0200

    Add files via upload

---
+++ CHPT-07-Functional-programming-with-comprehensions/Exer-30-Flatten-a-list/flatten_BTE_1.py
@@ -0,0 +1,18 @@
+
+'''
+Write a version of the flatten function mentioned earlier called flatten_odd _ints.
+It’ll do the same thing as flatten, but the output will only contain odd integers.
+Inputs that are neither odd nor integers should be excluded. Inputs containing strings
+that could be converted to integers should be converted; other strings should be excluded.
+'''
+
+def odd_int_fltr(the_lst):
+    return [int(float(item)) \
+            for sub_lst in the_lst \
+            for item in sub_lst \
+            if (isinstance(item, (int, float)) and item % 2) or \
+            (isinstance(item, str) and (item.isdigit() or item.replace('.', '').isdigit()) \
+             and float(item) % 2)]
+
+
+print(odd_int_fltr( [ [7, 9, 1], {}, ['Z', 7, '8.5'] ] ))
