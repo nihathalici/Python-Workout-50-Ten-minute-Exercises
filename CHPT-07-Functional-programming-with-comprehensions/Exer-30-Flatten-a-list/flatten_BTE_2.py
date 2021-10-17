dc2ef9c4ac471655f61a5a8e9677d3d6ee80a1c0
Nihat Halici <nihat.halici@gmail.com>
2021-10-17 15:58:09 +0200

    Add files via upload

---
+++ CHPT-07-Functional-programming-with-comprehensions/Exer-30-Flatten-a-list/flatten_BTE_2.py
@@ -0,0 +1,20 @@
+
+'''
+Define a dict that represents the children and grandchildren in a family.
+(See figure 7.1 for a graphic representation.) Each key will be a child’s name,
+and each value will be a list of strings representing their children
+(i.e., the family’s grandchildren). Thus the dict
+{'A':['B', 'C', 'D'], 'E':['F', 'G']} means
+that A and E are siblings; A’s children are B, C, and D;
+and E’s children are F andG. Use a list comprehension to create a list
+of the grandchildren’s names.
+'''
+
+my_fml_tr = {'A':['B','C','D'], 'E':['F','G']}
+
+def fml_di(fml_tr):
+    return [item
+            for k in fml_tr
+            for item in fml_tr[k]]
+    
+print( fml_di(my_fml_tr) )
---
+++ CHPT-07-Functional-programming-with-comprehensions/Exer-30-Flatten-a-list/flatten_BTE_3.py
@@ -0,0 +1,18 @@
+
+'''
+Redo this exercise, but replace each grandchild’s name (currently a string)
+with a dict. Each dict will contain two name-value pairs, name and age. Produce
+a listof the grandchildren’s names, sorted by age, from eldest to youngest.
+'''
+
+my_fml_tr = {'A':{'B':12,'C':7,'D':13}, 'E':{'F':14,'G':1}}
+
+def sort_by_age(fml_tr):
+    
+    grandch = {in_k:fml_tr[k][in_k]
+               for k in fml_tr
+               for in_k in fml_tr[k]}
+    print(grandch)
+    return sorted(grandch, key=lambda x: grandch[x])
+
+print(sort_by_age(my_fml_tr))
