d34c61351929889ae896b816b63a48d0581836e7
Nihat Halici <nihat.halici@gmail.com>
2021-10-21 02:07:22 -0700

    Add files via upload

---
+++ CHPT-07-Functional-programming-with-comprehensions/Exer-32-Flip-a-dict/flipped_dict_BTE_2.py
@@ -0,0 +1,16 @@
+
+'''
+Create a dict whose keys are filenames and whose values are the lengths of the
+files. The input can be a list of files from os.listdir (http://mng.bz/YreB)
+or glob.glob (http://mng.bz/044N).
+'''
+
+from pathlib import Path
+
+def cnt_fl_di(cnt_pth):
+    fl_di = {}
+    my_path = Path(cnt_pth)
+    fl_di = {fl.name:fl.stat().st_size for fl in my_path.iterdir() if fl.is_file()}
+    return fl_di
+
+print(cnt_fl_di('my_data_dir'))
