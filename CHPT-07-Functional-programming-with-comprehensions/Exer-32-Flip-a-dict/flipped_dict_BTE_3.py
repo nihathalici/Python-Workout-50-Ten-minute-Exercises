6be7e4897e5cc8fca758b23b430580a076d9fb30
Nihat Halici <nihat.halici@gmail.com>
2021-10-21 02:09:20 -0700

    Add files via upload

---
+++ CHPT-07-Functional-programming-with-comprehensions/Exer-32-Flip-a-dict/flipped_dict_BTE_3.py
@@ -0,0 +1,12 @@
+
+'''
+Find a configuration file in which the lines look like "name=value."
+Use a dict comprehension to read from the file, turning each line
+into a key-value pair.
+'''
+
+def di_frm_fl(fname):
+    with open(fname) as f:
+        return {l.split(':')[0]:l.split(':')[1] for l in f}
+
+print(di_frm_fl('my_data_dir/config_file'))
