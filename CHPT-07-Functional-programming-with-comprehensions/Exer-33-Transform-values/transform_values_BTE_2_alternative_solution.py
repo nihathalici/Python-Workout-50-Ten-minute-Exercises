faeb528ef70f0daeb01ffa6d90ac71d7e2c0ea84
Nihat Halici <nihat.halici@gmail.com>
2021-10-23 01:47:08 -0700

    Add files via upload

---
+++ CHPT-07-Functional-programming-with-comprehensions/Exer-33-Transform-values/transform_values_BTE_2_alternative_solution.py
@@ -0,0 +1,21 @@
+'''
+Use a dict comprehension to create a dict in which the keys are usernames
+and the values are (integer) user IDs, based on a Unix-style /etc/passwd file.
+
+Hint: in a typical /etc/passwd file, the usernames are the first field
+in a row (i.e., index 0), and the user IDs are the third field in a row
+(i.e., index 2). If you need to download a sample /etc/passwd file,
+you can get it from http://mng.bz/ 2XXg. Note that this sample file contains
+comment lines, meaning that you’ll need to remove them when creating your dict.
+'''
+
+def tr_vl_file(file):
+    d = {}
+    with open(file) as f:
+        di = {line.split(':')[0]:line.split(':')[2]
+              for line in f
+              if not line.startswith('#')}
+    return di
+
+print(tr_vl_file('user_db_with_com.txt'))
+
---
+++ CHPT-07-Functional-programming-with-comprehensions/Exer-33-Transform-values/transform_values_BTE_3.py
@@ -0,0 +1,21 @@
+'''
+Write a function that takes a directory name (i.e., a string) as an argument.
+The function should return a dict in which the keys are the names of files
+in that directory, and the values are the file sizes. You can use os.listdir
+or glob.glob to get the files, but because only regular files have sizes,
+you’ll want to filter the results using methods from os.path. To determine
+the file size, you can use os.stat or (if you prefer) just check the length
+of the string resulting from reading the file.
+'''
+
+from pathlib import Path
+
+def tr_vl_dir(a_path):
+    di = {}
+    values = Path(a_path)
+    return {item.name:item.stat().st_size
+            for item in values.iterdir()
+            if item.is_file()}
+
+
+print(tr_vl_dir('.'))
