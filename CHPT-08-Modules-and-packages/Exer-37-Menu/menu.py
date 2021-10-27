26f067bee48bb6a1fba212f4408d57b1aa47ffe6
Nihat Halici <nihat.halici@gmail.com>
2021-10-27 03:19:49 -0700

    Add files via upload

---
+++ CHPT-08-Modules-and-packages/Exer-37-Menu/menu.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""Function that takes keyword arguments. The value
+associated with each key is a function taking zero arguments.
+The user is asked to enter input.
+If the input matches a keyword, then the associated function
+is invoked, and its return value is returned to the user.
+If the input doesn't match a keywork, the user is asked to
+try again.
+"""
+
+
+def menu(**options):
+
+    while True:
+        option_string = '/'.join(sorted(options))
+        choice = input(f'Enter an option ({option_string}): ')
+        if choice in options:
+            return options[choice]()
+
+        print('Not a valid option')
+
+
