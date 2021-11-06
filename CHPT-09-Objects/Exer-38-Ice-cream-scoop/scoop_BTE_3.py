def74e910b7387d09caafc0fcf011c2d3cdd8259
Nihat Halici <nihat.halici@gmail.com>
2021-11-06 12:46:19 +0100

    Create scoop_BTE_3.py

---
+++ CHPT-09-Objects/Exer-38-Ice-cream-scoop/scoop_BTE_3.py
@@ -0,0 +1,14 @@
+
+'''
+Create a new LogFile class that expects to be initialized with a filename. Inside of __init__,
+open the file for writing and assign it to an attribute, file, that sits on the instance.
+Check that it’s possible to write to the file via the file attribute.
+'''
+
+class LogFile():
+    def __init__(self, fname):
+        self.file = open(fname, 'w')
+
+f1 = LogFile('text1.txt')
+f2 = LogFile('text2.txt')
+f3 = LogFile('text3.txt')
