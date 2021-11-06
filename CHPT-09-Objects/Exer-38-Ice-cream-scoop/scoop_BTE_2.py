e3934c2eb73d8ae028d3d516fa8a2716da344679
Nihat Halici <nihat.halici@gmail.com>
2021-11-06 11:48:17 +0100

    Create scoop_BTE_2.py

---
+++ CHPT-09-Objects/Exer-38-Ice-cream-scoop/scoop_BTE_2.py
@@ -0,0 +1,18 @@
+'''
+Modify the Beverage class, such that you can create a new instance specifying the name,
+and not the temperature. If you do this, then the temperature should have a default value
+of 75 degrees Celsius. Create several beverages and double-check that the temperature has
+this default when not specified.
+'''
+
+class Beverage():
+    def __init__(self, name, temp=75):
+        self.name = name
+        self.temp = temp
+
+b1 = Beverage('coffee', 90)
+b2 = Beverage('ice tea', 5)
+b3 = Beverage('water')
+
+for item in [b1, b2, b3]:
+    print('The beverage {} is {}.'.format(item.name, item.temp))
