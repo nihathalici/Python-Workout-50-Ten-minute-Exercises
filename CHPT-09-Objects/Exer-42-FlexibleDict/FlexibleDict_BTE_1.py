0d6157b2baf03c7794f7c2d54f5bc0c9f30a2316
Nihat Halici <nihat.halici@gmail.com>
2021-11-15 12:23:49 +0100

    Add files via upload

---
+++ CHPT-09-Objects/Exer-42-FlexibleDict/FlexibleDict_BTE_1.py
@@ -0,0 +1,21 @@
+'''
+With FlexibleDict, we allowed the user to use any key, but were then
+flexible with the retrieval. Implement StringKeyDict, which converts
+its keys into strings as part of the assignment. Thus, immediately
+after saying skd[1] = 10, you would be able to then say skd['1'] and
+get the value of 10 returned. This can come in handy if you’ll be reading
+keys from a file and won’t be able to distinguish between strings and integers.
+'''
+
+
+class StringKeyDict(dict):
+    def __setitem__(self, key, value):
+        dict.__setitem__(self, str(key), value)
+
+skd = StringKeyDict()
+
+skd[1] = 10
+print(skd['1'])
+print(type(skd.keys()))
+
+
