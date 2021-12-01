69488227e3eb2ae90b46cf6ae990eefe589cc265
Nihat Halici <nihat.halici@gmail.com>
2021-12-01 19:10:56 +0100

    Create circle.py

---
+++ CHPT-10-Iterators-and-generators/Exer-47-Circle/circle.py
@@ -0,0 +1,38 @@
+
+class CircleIterator:
+    def __init__(self, data, maxtimes):
+        self.data = data
+        self.maxtimes = maxtimes
+        self.index = 0
+
+    def __next__(self):
+        if self.index >= self.maxtimes:
+            raise StopIteration
+
+        value = self.data[self.index % len(self.data)]
+        self.index += 1
+        return value
+    
+
+class Circle:
+    def __init__(self, data, maxtimes):
+        self.data = data
+        self.maxtimes = maxtimes
+        self.index = 0
+
+    def __iter__(self):
+        return CircleIterator(self.data, self.maxtimes)
+
+        
+
+c = Circle('abcd', 7)
+
+
+# abcdabc
+print('** A **')
+for one_item in c:
+    print(one_item)
+
+print('** B **')
+for one_item in c:
+    print(one_item)
