be4e79a09baa49b3c89ea7b058dda5ab8bfab085
Nihat Halici <nihat.halici@gmail.com>
2021-12-02 08:14:31 +0100

    Add files via upload

---
+++ CHPT-10-Iterators-and-generators/Exer-47-Circle/circle_BTE_2.py
@@ -0,0 +1,21 @@
+
+'''
+Implement Circle as a generator function, rather than as a class.
+'''
+
+def circle(data, max_times):
+    for index in range(max_times):
+        yield data[index % len(data)]
+
+
+c = circle('abcd', 7)
+
+
+# abcdabc
+#print('** A **')
+for one_item in c:
+    print(one_item)
+
+#print('** B **')
+#for one_item in c:
+#    print(one_item)
