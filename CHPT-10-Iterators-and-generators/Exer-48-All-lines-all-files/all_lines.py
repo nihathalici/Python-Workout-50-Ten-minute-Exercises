ad675b4f0624cc66f90fef8fd7b7b9479e29b72c
Nihat Halici <nihat.halici@gmail.com>
2021-12-03 10:52:13 +0100

    Create all_lines.py

---
+++ CHPT-10-Iterators-and-generators/Exer-48-All-lines-all-files/all_lines.py
@@ -0,0 +1,20 @@
+'''
+Write an iterator (as a generator function) that returns each line
+from each file in a named directory.
+
+Any file that cannot be opened, for whatever reason, is ignored.
+'''
+
+import os
+
+def all_lines(path):
+    for filename in os.listdir(path):
+        full_filename = os.path.join(path, filename)
+        try:
+            for line in open(full_filename):
+                yield line
+        except OSError:
+            pass
+
+for one_line in all_lines('txt_files'):
+    print(one_line)
