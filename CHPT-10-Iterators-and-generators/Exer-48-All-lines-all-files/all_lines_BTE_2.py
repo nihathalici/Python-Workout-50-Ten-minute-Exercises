74fd1e0cc1ce909e1479fbc6739105bc893e4cf3
Nihat Halici <nihat.halici@gmail.com>
2021-12-04 10:56:25 +0100

    Add files via upload

---
+++ CHPT-10-Iterators-and-generators/Exer-48-All-lines-all-files/all_lines_BTE_2.py
@@ -0,0 +1,39 @@
+'''
+The current version of all_lines returns all of the lines from the first file,
+then all of the lines from the second file, and so forth. Modify the function
+such that it returns the first line from each file, and then the second line
+from each file, until all lines from all files are returned. When you finish
+printing lines from shorter files, ignore those files while continuing to
+display lines from the longer files.
+'''
+
+import os
+
+def open_file_safely(filename):
+    try:
+        return open(filename)
+    except OSError:
+        return None
+
+
+def all_lines_alt(path):
+    all_files = [open_file_safely(os.path.join(path, filename))
+                 for filename in os.listdir(path)]
+
+    while all_files:
+        for one_file in all_files:
+            if one_file is None:
+                all_files.remove(one_file)
+                continue
+
+            one_line = one_file.readline()
+
+            if one_line:
+                yield one_line
+            else:
+                all_files.remove(one_file)
+
+
+
+for one_line in all_lines_alt('txt_files'):
+    print(one_line)
---
+++ CHPT-10-Iterators-and-generators/Exer-48-All-lines-all-files/all_lines_BTE_3.py
@@ -0,0 +1,23 @@
+
+'''
+Modify all_lines such that it takes two arguments—a directory name, and a string.
+Only those lines containing the string (i.e., for which you can say s in line) should
+be returned. If you know how to work with regular expressions and Python’s re module,
+then you could even make the match conditional on a regular expression.
+'''
+
+import os
+
+def all_lines(path, s):
+    for filename in os.listdir(path):
+        full_filename = os.path.join(path, filename)
+        try:
+            for line in open(full_filename):
+                if s in line:
+                    yield line
+        except OSError:
+            pass
+
+for one_line in all_lines('txt_files', 'Python'):
+    print(one_line)
+
