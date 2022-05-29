a760c05106a4f8cfddf49e9668f30f02f86f72eb
Nihat Halici <nihat.halici@gmail.com>
2022-05-29 10:53:54 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-01-Numeric-Types/Exer-01-Number-guessing-game/Number-guessing-game.py
@@ -0,0 +1,20 @@
+import random
+
+def guessing_game():
+
+    answer = random.randint(0, 100)
+
+    while True:
+        user_guess = int(input("What is your guess? "))
+
+        if user_guess == answer:
+            print("Right! The answer is {}".format(user_guess))
+            break
+
+        if user_guess < answer:
+            print("Your guess of {} is too low!".format(user_guess))
+
+        else:
+            print("Your guess of {} is too high!".format(user_guess))
+
+guessing_game()
