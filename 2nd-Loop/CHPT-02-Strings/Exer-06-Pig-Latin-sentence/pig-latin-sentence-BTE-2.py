22f66768bbb4e5182d7427595567eb6964f0d31c
Nihat Halici <nihat.halici@gmail.com>
2022-06-04 09:55:18 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-02-Strings/Exer-06-Pig-Latin-sentence/pig-latin-sentence-BTE-2.py
@@ -0,0 +1,15 @@
+
+'''
+Write a function that transposes a list of strings, in which each string contains
+multiple words separated by whitespace. Specifically, it should perform in such a way
+that if you were to pass the list ['abc def ghi', 'jkl mno pqr', 'stu vwx yz'] to the function,
+it would return ['abc jkl stu', 'def mno vwx', 'ghi pqr yz'].
+'''
+
+old_list = ['abc def ghi', 'jkl mno pqr', 'stu vwx yz']
+
+lists = [x.split() for x in old_list]
+
+print( [ " ".join(x) for x in zip(*lists) ])
+
+
