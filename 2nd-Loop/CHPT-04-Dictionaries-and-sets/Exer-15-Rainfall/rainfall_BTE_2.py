c2d241b1e4802e96fadd054fc5406d6490c94166
Nihat Halici <nihat.halici@gmail.com>
2022-06-17 07:52:56 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-04-Dictionaries-and-sets/Exer-15-Rainfall/rainfall_BTE_2.py
@@ -0,0 +1,17 @@
+'''
+Open a log file from a Unix/Linux system—for example, one from the Apache
+server. For each response code (i.e., three-digit code indicating the HTTP
+request’s success or failure), store a list of IP addresses that generated
+that code.
+'''
+
+def log_chq(fname):
+    rsp_code = {}
+    with open(fname, 'r') as f:
+        for l in f:
+            l = l.split()
+            print(l)
+            rsp_code[l[8]] = rsp_code.get(l[8], []) + l[:1]
+    return rsp_code
+
+print(log_chq('log_file.txt'))
