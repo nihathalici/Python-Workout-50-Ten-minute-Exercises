6848f9101c931d4679fdb81351d754d17472040f
Nihat Halici <nihat.halici@gmail.com>
2022-06-24 10:39:04 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-05-Files/Exer-19-etc-passwd-to-dict/passwd_to_dict.py
@@ -0,0 +1,10 @@
+
+
+def passwd_to_dict(fname):
+    return {
+        one_line.split(':')[0] : int(one_line.split(':')[2])
+        for one_line in open(fname)
+        if not one_line.startswith(('#', '\n'))
+    }
+
+print(passwd_to_dict('passwd.txt'))
---
+++ 2nd-Loop/CHPT-05-Files/Exer-19-etc-passwd-to-dict/passwd_to_dict_alternative_solution.py
@@ -0,0 +1,13 @@
+
+def passwd_to_dict(fname):
+    users = {}
+    with open(fname) as passwd:
+        for line in passwd:
+            if not line.startswith(('#', '\n')):
+                user_info = line.split(':')
+                users[user_info[0]] = int(user_info[2])
+    return users
+
+print(passwd_to_dict('passwd.txt'))
+
+
