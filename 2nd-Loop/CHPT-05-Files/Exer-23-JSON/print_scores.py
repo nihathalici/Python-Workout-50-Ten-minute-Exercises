e904f1d5b39c637fb98ba33e4e5939ea4ccf0516
Nihat Halici <nihat.halici@gmail.com>
2022-07-05 08:26:25 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-05-Files/Exer-23-JSON/print_scores.py
@@ -0,0 +1,28 @@
+
+import json
+import glob
+
+def print_scores(dirname):
+    scores = {}
+    for filename in glob.glob(f'{dirname}/*.json'):
+        scores[filename] = {}
+        with open(filename) as infile:
+            for result in json.load(infile):
+                for subject, score in result.items():
+                    scores[filename].setdefault(subject, [])
+                    scores[filename][subject].append(score)
+
+    for one_class in scores:
+        print(one_class)
+        for subject, subject_scores in scores[one_class].items():
+            min_score = min(subject_scores)
+            max_score = max(subject_scores)
+            average_score = (sum(subject_scores) / len(subject_scores))
+
+            print(subject)
+            print(f'\tmin {min_score}')
+            print(f'\tmax {max_score}')
+            print(f'\taverage {average_score}')
+
+print_scores('scores')
+
