a8571fdacb31a1f37f7e5052b4380936410a0b0a
Nihat Halici <nihat.halici@gmail.com>
2022-07-07 07:46:41 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-05-Files/Exer-24-Reverse-lines/reverse_lines.py
@@ -0,0 +1,7 @@
+
+def reverse_lines(infname, outfname):
+    with open(infname) as inf, open(outfname, 'w') as outf:
+        for one_line in inf:
+            outf.write(f'{one_line.rstrip()[::-1]}\n')
+
+reverse_lines('input_text.txt', 'output_text3.txt')
