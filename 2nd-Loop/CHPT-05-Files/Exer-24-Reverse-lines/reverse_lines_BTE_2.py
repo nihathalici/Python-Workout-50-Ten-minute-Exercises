a7d55edbefedbe2512a10ea933ad445541be86ab
Nihat Halici <nihat.halici@gmail.com>
2022-07-08 08:00:20 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-05-Files/Exer-24-Reverse-lines/reverse_lines_BTE_2.py
@@ -0,0 +1,24 @@
+
+'''
+Given an existing text file, create two new text files. The new files will each contain
+the same number of lines as the input file. In one output file, you’ll write all of the
+vowels (a, e, i, o, and u) from the input file. In the other, you’ll write all of the
+consonants. (You can ignore punctuation and whitespace.)
+'''
+
+def en_de_crypt(f_in='data_vwl.txt', f_vow='vwls.txt', f_con='cons.txt'):
+    all_vwl = 'aeiou'
+    with open(f_in) as rd_f, open(f_vow, 'w') as vwl_f, open(f_con, 'w') as con_f:
+        for l in rd_f:
+            for c in l:
+                if c in all_vwl:
+                    vwl_f.write(c)
+                elif c.isalpha() and not(c in all_vwl):
+                    con_f.write(c)
+            else:
+                vwl_f.write('\n')
+                con_f.write('\n')
+
+en_de_crypt()
+
+
---
+++ 2nd-Loop/CHPT-05-Files/Exer-24-Reverse-lines/reverse_lines_BTE_3.py
@@ -0,0 +1,38 @@
+
+'''
+The final field in /etc/passwd is the shell, the Unix command interpreter that’s
+invoked when a user logs in. Create a file, containing one line per shell, in
+which the shell’s name is written, followed by all of the usernames
+that use the shell; for example
+/bin/bash:root, jci, user, reuven, atara
+/bin/sh:spamd, gitlab
+'''
+"""
+def rd_shll(f_in='linux-etc-passwd.txt', f_out='output6.txt'):
+    shll_usr = {}
+    with open(f_in) as f_rd, open(f_out, 'w') as f_wr:
+        for l in f_rd:
+            if not l.startswith('#'):
+                l = l.rstrip().split(':')
+                shll_usr[l[-1]] = shll_usr.get(l[-1],[]) + [l[0]]
+        for k in shll_usr:
+            f_wr.write(f ' {k} : { ",".join( shll_usr[k] ) }\n')
+    
+
+rd_shll()
+
+"""
+###
+
+def rd_shll(f_in='linux-etc-passwd.txt', f_out='output7.txt'):
+    shll_usr = {}
+    with open(f_in) as f_rd, open(f_out, 'w') as f_wr:
+        for l in f_rd:
+            if not l.startswith('#'):
+                l = l.rstrip().split(':')
+                shll_usr[l[-1]] = shll_usr.get(l[-1],[]) + [l[0]]
+        for k in shll_usr:
+            f_wr.write(f'{k} : { ",".join( shll_usr[k] ) }\n' )
+
+rd_shll()
+
