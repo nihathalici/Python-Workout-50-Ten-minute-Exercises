0b42574d59f733026e37e6f6fea9db7e346ac1cb
Nihat Halici <nihat.halici@gmail.com>
2022-07-13 11:11:30 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-06-Functions/Exer-27-Password-generator/create_password_generator.py
@@ -0,0 +1,16 @@
+
+import random
+
+def create_p_gen(chars):
+
+    def create_p(length):
+        output = []
+        for i in range(length):
+            output.append(random.choice(chars))
+        return ''.join(output)
+
+    return create_p
+
+f = create_p_gen('abcdefghij!@#$%')
+
+print(f(10))
