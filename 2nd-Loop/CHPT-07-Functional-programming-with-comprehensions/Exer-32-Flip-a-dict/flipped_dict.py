14dc2cd86e8f45cf1921ab859765d090af6b020e
Nihat Halici <nihat.halici@gmail.com>
2022-07-20 08:46:17 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-07-Functional-programming-with-comprehensions/Exer-32-Flip-a-dict/flipped_dict.py
@@ -0,0 +1,11 @@
+
+
+my_dict = {'a':1, 'b':2, 'c':3, 'd':3}
+
+def flipped_dict(a_dict):
+    return {
+        value : key
+        for key, value in a_dict.items()
+    }
+
+print(flipped_dict(my_dict))
