1b43e9aa804b2fb2732133cc9061213a79a34b43
Nihat Halici <nihat.halici@gmail.com>
2022-07-22 09:06:09 +0200

    Update get_sv_BTE_3.py

--- CHPT-07-Functional-programming-with-comprehensions/Exer-34-Almost-supervocalic-words/get_sv_BTE_3.py
+++ CHPT-07-Functional-programming-with-comprehensions/Exer-34-Almost-supervocalic-words/get_sv_BTE_3.py
@@ -1,11 +1,18 @@
-'''
-Given a text file, what are the lengths of the different words?
-Return a set of different word lengths in the file.
-'''
 
-def wo_lngth(a_file):
-    return {len(wrd.strip())
-            for wrd in open(a_file)}
+"""
+Create a list whose elements are strings—the names of people in your family.
+Now use a set comprehension (and, better yet, a nested set comprehension) to
+find which letters are used in your family members’ names.
+"""
 
-print(wo_lngth('words.txt'))
+# Author's solution
 
+import string
+
+def letters_in_names(list_of_names):
+    return {one_letter
+            for one_letter in ''.join(list_of_names)
+            if one_letter in string.ascii_letters}
+
+my_lst_names = ['Iris', 'Angel', 'Carlos', 'Mariate', 'Miguel']
+print(letters_in_names(my_lst_names))
