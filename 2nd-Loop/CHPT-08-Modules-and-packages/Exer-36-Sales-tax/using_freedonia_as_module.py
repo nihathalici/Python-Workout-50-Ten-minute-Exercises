56bc3e772e086b6f2fb04721726a548f7c8e72f4
Nihat Halici <nihat.halici@gmail.com>
2022-07-29 08:07:03 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-08-Modules-and-packages/Exer-36-Sales-tax/using_freedonia_as_module.py
@@ -0,0 +1,8 @@
+
+from freedonia import calculate_tax
+
+tax_at_12noon = calculate_tax(100, 'Harpo', 12)
+tax_at_9pm = calculate_tax(100, 'Harpo', 21)
+
+print(f'You owe a total of: {tax_at_12noon}')
+print(f'You owe a total of: {tax_at_9pm}')
