b66ba5be086d7d25bf0d3a074e824690b4f47fd6
Nihat Halici <nihat.halici@gmail.com>
2022-07-31 18:03:18 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-09-Objects/Exer-38-Ice-cream-scoop/scoop.py
@@ -0,0 +1,11 @@
+
+class Scoop:
+    def __init__(self, flavor):
+        self.flavor = flavor
+
+s1 = Scoop('chocolate')
+s2 = Scoop('vanilla')
+s3 = Scoop('persimmon')
+
+for one_scoop in [s1, s2, s3]:
+    print(one_scoop.flavor)
