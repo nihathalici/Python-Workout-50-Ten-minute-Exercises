bb60510759bc89299cca1cedcbbc2884469cd79c
Nihat Halici <nihat.halici@gmail.com>
2022-08-06 11:48:43 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-09-Objects/Exer-42-FlexibleDict/FlexibleDict_BTE_2.py
@@ -0,0 +1,30 @@
+'''
+The RecentDict class works just like a dict, except that it contains a user-defined
+number of key-value pairs, which are determined when the instance is created. In a
+RecentDict(5), only the five most recent key-value pairs are kept; if there are more
+than five pairs, then the oldest key is removed, along with its value.
+
+Note: your implementation could take into account the fact that modern dicts store
+their key-value pairs in chronological order.
+'''
+
+class RecentDict(dict):
+    def __init__(self, maxsize):
+        super().__init__()
+        self.maxsize = maxsize
+
+    def __setitem__(self, key, value):
+        dict.__setitem__(self, str(key), value)
+
+        if len(self) > self.maxsize:
+            self.pop(list(self.keys())[0])
+        
+rd = RecentDict(5)
+rd[1] = 100
+rd[2] = 200
+rd[3] = 300
+rd[4] = 400
+rd[5] = 500
+rd[6] = 600
+
+print(rd)
---
+++ 2nd-Loop/CHPT-09-Objects/Exer-42-FlexibleDict/FlexibleDict_BTE_3.py
@@ -0,0 +1,20 @@
+'''
+The FlatList class inherits from list and overrides the append method.
+If append is passed an iterable, then it should add each element of the
+iterable separately. This means that fl.append([10, 20, 30]) would not
+add the list [10, 20, 30] to fl, but would rather add the individual
+integers 10, 20, and 30. You might want to use the built-in iter
+function (http://mng.bz/Qy2G) to determine whether the passed argument
+is indeed iterable.
+'''
+
+class FlatList(list):
+    def append(self, new_value):
+        try:
+            for one_item in new_value:
+                list.append(self, one_item)
+        except TypeError:
+            list.append(self, new_value)
+
+fl = FlatList([10, 20, 30])
+print(fl)
