2b230d7c499c7ff23cdb8be4dfe4bda07f1651a1
Nihat Halici <nihat.halici@gmail.com>
2022-08-06 11:51:30 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-09-Objects/Exer-43-Animals/animals_BTE_1.py
@@ -0,0 +1,62 @@
+'''
+Instead of each animal class inheriting directly, from Animal, define
+several new classes, ZeroLeggedAnimal, TwoLeggedAnimal, and FourLeggedAnimal,
+all of which inherit from Animal, and dictate the number of legs on each instance.
+Now modify Wolf, Sheep, Snake, and Parrot such that each class inherits from one
+of these new classes, rather than directly from Animal. How does this affect
+your method definitions?
+'''
+
+class Animal:
+    def __init__(self, color):
+        self.color = color
+        self.species = self.__class__.__name__
+    
+    def __repr__(self):
+        return f'{self.color} {self.species}, {self.number_of_legs} legs'
+
+class ZeroLeggedAnimal(Animal):
+    def __init__(self, color):
+        super().__init__(color)
+        self.number_of_legs = 0
+
+class TwoLeggedAnimal(Animal):
+    def __init__(self, color):
+        super().__init__(color)
+        self.number_of_legs = 2
+
+class FourLeggedAnimal(Animal):
+    def __init__(self, color):
+        super().__init__(color)
+        self.number_of_legs = 4
+
+class Wolf(FourLeggedAnimal):
+    def __init__(self, color):
+        super().__init__(color)
+
+class Sheep(FourLeggedAnimal):
+    def __init__(self, color):
+        super().__init__(color)
+
+class Snake(ZeroLeggedAnimal):
+    def __init__(self, color):
+        super().__init__(color)
+
+class Parrot(TwoLeggedAnimal):
+    def __init__(self, color):
+        super().__init__(color)
+
+
+unknown_four_legged_animal = FourLeggedAnimal('pink')
+wolf = Wolf('black')
+sheep1 = Sheep('black')
+sheep2 = Sheep('white')
+snake = Snake('brown')
+parrot = Parrot('green')
+
+print(wolf)
+print(sheep1)
+print(sheep2)
+print(snake)
+print(parrot)
+print(unknown_four_legged_animal)
