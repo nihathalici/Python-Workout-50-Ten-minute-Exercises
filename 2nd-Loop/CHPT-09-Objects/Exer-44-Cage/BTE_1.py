ce0e677f0fec4d55e874003273d721af8b6bbde4
Nihat Halici <nihat.halici@gmail.com>
2022-08-19 08:52:25 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-09-Objects/Exer-44-Cage/BTE_1.py
@@ -0,0 +1,27 @@
+'''
+As you can see, there are no limits on how many animals can potentially be put
+into a cage. Just as we put a limit of three scoops in a Bowl and five in a
+BigBowl, you should similarly create Cage and BigCage classes that limit the
+number of animals that can be placed there.
+'''
+class Animal():
+    pass
+
+class Wolf(Animal):
+    pass
+
+class Sheep(Animal):
+    pass
+
+class Snake(Animal):
+    pass
+
+class Parrot(Animal):
+    pass
+
+class Cage():
+    pass
+
+class BigCage(Cage):
+    pass
+
