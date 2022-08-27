f7264fcf92643257b70569ad72a9ff69618d1b1b
Nihat Halici <nihat.halici@gmail.com>
2022-08-27 12:14:26 +0200

    Update BTE_3.py

--- 2nd-Loop/CHPT-09-Objects/Exer-44-Cage/BTE_3.py
+++ 2nd-Loop/CHPT-09-Objects/Exer-44-Cage/BTE_3.py
@@ -42,11 +42,56 @@ class Parrot(Animal):
     def __init__(self, color):
         super().__init__(color, 2)
 
+animal_safety = {Wolf: [Wolf, Snake, Parrot],
+                 Sheep: [Sheep, Snake, Parrot],
+                 Snake: [Wolf, Sheep],
+                 Parrot: [Wolf, Sheep]}
+
 class DangerousAssignmentError(Exception):
     pass
 
 class Cage():
-    pass
+    def __init__(self, id_number):
+        self.id_number = id_number
+        self.animals = []
+    
+    def add_animals(self, *animals):
+        for one_animal in animals:
+            for one_current_resident in self.animals:
+                if type(one_animal) not in animal_safety[type(one_current_resident)]:
+                    raise DangerousAssignmentError(
+                        f'You cannot put a {type(one_animal)} with a {type(one_current_resident)}!')
+            self.animals.append(one_animal)
+    
+    def __repr__(self):
+        output = f'Cage {self.id_number}\n'
+        output += '\n'.join('\t' + str(animal)
+                            for animal in self.animals)
+        return output
 
 class BigCage(Cage):
     pass
+
+wolf = Wolf('black')
+sheep1 = Sheep('white')
+sheep2 = Sheep('black')
+sheep3 = Sheep('blue')
+snake = Snake('brown')
+parrot = Parrot('green')
+
+c1 = Cage(1)
+c1.add_animals(wolf)
+
+c2 = Cage(2)
+c2.add_animals(snake, sheep1)
+
+bc = BigCage(3)
+bc.add_animals(wolf, sheep1, sheep2, sheep3, snake)
+
+bc2 = BigCage(4)
+bc2.add_animals(wolf, sheep1, sheep2, sheep3, snake, parrot)
+
+print(c1)
+print(c2)
+print(bc)
+print(bc2)
