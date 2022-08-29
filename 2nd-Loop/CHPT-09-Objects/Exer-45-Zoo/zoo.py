8d831eff536d1c04783bd433a3b27a1d5a5b54c4
Nihat Halici <nihat.halici@gmail.com>
2022-08-29 11:09:54 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-09-Objects/Exer-45-Zoo/zoo.py
@@ -0,0 +1,23 @@
+class Animal():
+    pass
+
+class Wolf(Animal):
+    pass
+
+class Sheep(Animal):
+    pass
+
+class Snake(Animal):
+    pass
+
+class Wolf(Animal):
+    pass
+
+class Parrot(Animal):
+    pass
+
+class Cage():
+    pass
+
+class Zoo():
+    pass
