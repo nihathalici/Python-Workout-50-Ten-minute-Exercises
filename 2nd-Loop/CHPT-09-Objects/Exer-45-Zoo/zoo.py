94144b004636e3eca474236b928fead564cf1550
Nihat Halici <nihat.halici@gmail.com>
2022-08-30 09:20:54 +0200

    Update zoo.py

--- 2nd-Loop/CHPT-09-Objects/Exer-45-Zoo/zoo.py
+++ 2nd-Loop/CHPT-09-Objects/Exer-45-Zoo/zoo.py
@@ -1,23 +1,45 @@
 class Animal():
-    pass
+    def __init__(self, color, number_of_legs):
+        self.species = self.__class__.__name__
+        self.color = color
+        self.number_of_legs = number_of_legs
+    
+    def __repr__(self):
+        return f'{self.color} {self.species}, {self.number_of_legs} legs'
 
 class Wolf(Animal):
-    pass
+    def __init__(self, color):
+        super().__init__(color, 4)
 
 class Sheep(Animal):
-    pass
+    def __init__(self, color):
+        super().__init__(color, 4)
 
 class Snake(Animal):
-    pass
+    def __init__(self, color):
+        super().__init__(color, 0)
 
-class Wolf(Animal):
-    pass
+class Parrot(Animal):
+    def __init__(self, color):
+        super().__init__(color, 2)
 
 class Parrot(Animal):
     pass
 
 class Cage():
-    pass
+    def __init__(self, id_number):
+        self.id_number = id_number
+        self.animals = []
+    
+    def add_animals(self, *animals):
+        for one_animal in animals:
+            self.animals.append(one_animal)
+    
+    def __repr__(self):
+        output = f"Cage {self.id_number}\n"
+        output += '\n'.join('\t' + str(animal) 
+                             for animal in self.animals)
+        return output
 
 class Zoo():
     pass
