5353c2cd1783e557188b5f3cbe5e930a106c4f7f
Nihat Halici <nihat.halici@gmail.com>
2022-08-31 08:51:07 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-09-Objects/Exer-45-Zoo/zoo_BTE_1.py
@@ -0,0 +1,30 @@
+'''
+Modify animals_by_color such that it takes any number of colors. Animals
+having any of the listed colors should be returned. The method should raise
+an exception if no colors are passed.
+'''
+
+class Animal():
+    pass
+
+class Wolf(Animal):
+    pass
+
+class Sheep(Animal):
+    pass
+
+class Snake(Animal):
+    pass
+
+class Parrot(Animal):
+    pass
+
+class Cage():
+    pass
+
+class NoColorsPassedError(Exception):
+    pass
+
+class Zoo():
+    pass
+
