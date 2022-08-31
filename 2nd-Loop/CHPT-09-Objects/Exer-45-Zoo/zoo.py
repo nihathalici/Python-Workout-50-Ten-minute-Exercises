915f1bf24b8038382c6d3cc57398fd76334165f8
Nihat Halici <nihat.halici@gmail.com>
2022-08-31 08:50:11 +0200

    Update zoo.py

--- 2nd-Loop/CHPT-09-Objects/Exer-45-Zoo/zoo.py
+++ 2nd-Loop/CHPT-09-Objects/Exer-45-Zoo/zoo.py
@@ -23,9 +23,6 @@ class Parrot(Animal):
     def __init__(self, color):
         super().__init__(color, 2)
 
-class Parrot(Animal):
-    pass
-
 class Cage():
     def __init__(self, id_number):
         self.id_number = id_number
@@ -42,4 +39,51 @@ class Cage():
         return output
 
 class Zoo():
-    pass
+    
+    def __init__(self):
+        self.cages = []
+    
+    def add_cages(self, *cages):
+        for one_cage in cages:
+            self.cages.append(one_cage)
+    
+    def __repr__(self):
+        return '\n'.join(str(one_cage) 
+                         for one_cage in self.cages)
+    
+    def animals_by_color(self, color):
+        return [one_animal
+                for one_cage in self.cages
+                for one_animal in one_cage.animals
+                if one_animal.color == color]
+    
+    def animals_by_legs(self, number_of_legs):
+        return [one_animal
+                for one_cage in self.cages
+                for one_animal in one_cage.animals
+                if one_animal.number_of_legs == number_of_legs]
+    
+    def number_of_legs(self):
+        return sum(one_animal.number_of_legs
+                   for one_cage in self.cages
+                   for one_animal in one_cage.animals)
+
+wolf = Wolf('black')
+sheep = Sheep('white')
+snake = Snake('brown')
+parrot = Parrot('green')
+
+c1 = Cage(1)
+c1.add_animals(wolf, sheep)
+
+c2 = Cage(2)
+c2.add_animals(snake, parrot)
+
+z = Zoo()
+z.add_cages(c1, c2)
+
+print(z)
+print(z.animals_by_color('white'))
+print(z.animals_by_legs(4))
+print(z.number_of_legs())
+
