42063b198e762dee21dd096769faa73e0152a37f
Nihat Halici <nihat.halici@gmail.com>
2022-09-03 11:30:58 +0200

    Update zoo_BTE_1.py

--- 2nd-Loop/CHPT-09-Objects/Exer-45-Zoo/zoo_BTE_1.py
+++ 2nd-Loop/CHPT-09-Objects/Exer-45-Zoo/zoo_BTE_1.py
@@ -48,5 +48,51 @@ class NoColorsPassedError(Exception):
     pass
 
 class Zoo():
-    pass
+    def __init__(self):
+        self.cages = []
+    
+    def add_cages(self, *cages):
+        for one_cage in cages:
+            self.cages.append(one_cage)
+    
+    def __repr__(self):
+        return '\n'.join(str(one_cage)
+                         for one_cage in self.cages)
+    
+    def animals_by_color(self, *args):
+        if not args:
+            raise NoColorsPassedError
+        return [one_animal
+                for one_cage in self.cages 
+                for one_animal in one_cage.animals
+                if one_animal.color in args]
+    
+    def animals_by_legs(self, number_of_legs):
+        return [one_animal
+                for one_cage in self.cages
+                for one_animal in one_cage.animals
+                if one_animal.number_of_legs == number_of_legs]
+    
+    def number_of_legs(self):
+        return sum(one_animal.number_of_legs
+                   for one_cage in self.cages
+                   for one_animal in one_cage.animals)
 
+wolf = Wolf('black')
+sheep = Sheep('white')
+snake = Snake('brown')
+parrot = Parrot('green')
+
+c1 = Cage(1)
+c1.add_animals(wolf, sheep)
+
+c2 = Cage(2)
+c2.add_animals(snake, parrot)
+
+z = Zoo()
+z.add_cages(c1, c2)
+
+print(z)
+print(z.animals_by_color('white'))
+print(z.animals_by_legs(4))
+print(z.number_of_legs())
