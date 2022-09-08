c17086907c9086c9099c50406e6ef92f9a1be126
Nihat Halici <nihat.halici@gmail.com>
2022-09-08 08:26:35 +0200

    Update zoo_BTE_3.py

--- 2nd-Loop/CHPT-09-Objects/Exer-45-Zoo/zoo_BTE_3.py
+++ 2nd-Loop/CHPT-09-Objects/Exer-45-Zoo/zoo_BTE_3.py
@@ -8,25 +8,67 @@ the passed criteria.
 '''
 
 class Animal():
-    pass
+    def __init__(self, color, number_of_legs):
+        self.species = self.__class__.__name__
+        self.color = color
+        self.number_of_legs = number_of_legs
+    
+    def __repr__(self):
+        return f'{self.color} {self.scpecies}, {self.number_of_legs} legs'
+
+
 
 class Wolf(Animal):
-    pass
+    def __init__(self, color):
+        super().__init__(color, 4)
 
 class Sheep(Animal):
-    pass
+    def __init__(self, color):
+        super().__init__(color, 4)
 
 class Snake(Animal):
-    pass
+    def __init__(self, color):
+        super().__init__(color, 0)
 
 class Parrot(Animal):
-    pass
+    def __init__(self, color):
+        super().__init__(color, 2)
 
 class Cage:
-    pass
+    def __init__(self, id_number):
+        self.id_number = id_number
+        self.animals = []
+    
+    def add_animals(self, *animals):
+        for one_animal in animals:
+            self.animals.append(one_animal)
+    
+    def __repr__(self):
+        output = f'Cage {self.id_number}\n'
+        output += '\n'.join('\t' + str(animal) 
+                            for animal in self.animals)
+        return output
 
 class NoColorsPassedError(Exception):
     pass
 
 class Zoo:
-    pass
+    def __init__(self):
+        self.cages = []
+    
+    def add_cages(self, *cages):
+        for one_cage in cages:
+            self.cages.append(one_cage)
+    
+    def __repr__(self):
+        return '\n'.join(str(one_cage)
+                         for one_cage in self.cages)
+    
+    def animals_by(self, **kwargs):
+        print(f'{kwargs=}')
+        return [one_animal
+                for one_cage in self.cages
+                for one_animal in one_cage.animals
+                if (
+                    ('color' in kwargs and one_animal.color == kwargs['color']) and
+                    ('legs' in kwargs and one_animal.number_of_legs == kwargs['legs']))]
