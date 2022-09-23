63196c11f1b5edbc88ad365cd80d8d22eb525ad3
Nihat Halici <nihat.halici@gmail.com>
2022-09-23 14:19:03 +0200

    Add files via upload

---
+++ 2nd-Loop/CHPT-10-Iterators-and-generators/Exer-48-All-lines-all-files/all_lines_BTE_1.py
@@ -0,0 +1,30 @@
+'''
+Modify all_lines such that it doesn’t return a string with each iteration,
+but rather a tuple. The tuple should contain four elements: the name of the file,
+the current number of the file (from all those returned by os.listdir), the
+line number within the current file, and the current line.
+'''
+import os
+
+def all_lines_tuple(path):
+    for file_index, filename in enumerate(os.listdir(path)):
+        full_filename = os.path.join(path, filename)
+        try:
+            for line_index, line in enumerate(open(full_filename)):
+                yield (full_filename, file_index, line_index, line)
+        except OSError:
+            pass
+
+for one_line in all_lines_tuple('txt-files'):
+    print(one_line)
+
+
+
+
+
+
+
+
+
+
+
